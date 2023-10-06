f23c6b0642858fec3a87ecd49a74b9dcff2ff90e
Udonwajnr <udonwajnr10@gmail.com>
2023-10-06 19:29:15 +0100

    commit

--- src/services/article.js
+++ src/services/article.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+const rapidApiKey="1361b1dbc8msh6ae8b7622f0aa11p148feajsn8b9a52af9060"
 
 export const articleApi = createApi({
     reducerPath: 'articleApi',
